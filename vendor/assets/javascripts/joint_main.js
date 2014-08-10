3d3ff509f351d2f621cbf0a4daca17de459849c6
kNishide <kohichi.nishide@nts.ricoh.co.jp>
2014-08-10 18:34:08 +0900

    add new page

--- vendor/assets/javascripts/joint_main.js
+++ vendor/assets/javascripts/joint_main.js
@@ -3,30 +3,29 @@
     var graph = new joint.dia.Graph;
     var paper = new joint.dia.Paper({
       el: $('#myholder'),
-      width: 600,
-      height: 200,
+      width: $(window).width(),
+      height: 1000,
       model: graph,
       gridSize: 1
     });
- 
-    var rect = new joint.shapes.basic.Rect({
-      position: { x: 100, y: 30 },
-      size: { width: 100, height: 30 },
-      attrs: { rect: { fill: 'blue' }, text: { text: 'my box', fill: 'white' } }
-    });
 
-    rect.on('change:position', function(element) {
-      console.log(element.id, ':', element.get('position'));
-    });
- 
-    var rect2 = rect.clone();
-    rect2.translate(300);
- 
-    var link = new joint.dia.Link({
-      source: { id: rect.id },
-      target: { id: rect2.id }
-    });
-
-    graph.addCells([rect, rect2, link]);
+    paper.on('blank:pointerdblclick', function(evt, x, y) {
+      var rect = createNewPage(x, y);
+      graph.addCells([rect]);
+    })
   });
 })();
+
+function createNewPage(x, y) {
+  var rect = new joint.shapes.basic.Rect({
+    position: { x: x, y: y },
+    size: { width: 100, height: 130 },
+    attrs: { rect: { fill: 'gray' }, text: { text: 'Page', fill: 'white' } }
+  });
+
+  rect.on('change:position', function(element) {
+    console.log(element.id, ':', element.get('position'));
+  });
+
+  return rect;
+}
