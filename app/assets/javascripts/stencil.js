18f19fa660878d36d5bc5f6748147e8a4a4ae9c0
kNishide <kohichi.nishide@nts.ricoh.co.jp>
2014-08-17 11:46:56 +0900

    add test dialog

--- app/assets/javascripts/stencil.js
+++ app/assets/javascripts/stencil.js
@@ -1,3 +1,8 @@
 function handleDragStart(e) {
   this.style.opacity = '0.4';  // this / e.target is the source node.
 }
+
+function showTestScenarios() {
+  var message = "Test scenario: 画面遷移テストサンプル \nGiven スクリーンAが表示されている\nWhen ボタンを押す\nThen スクリーンBが表示されている\nWhen ボタンを押す\nThen スクリーンCが表示されている\nWhen ボタンを押す";
+  alert(message);
+}
